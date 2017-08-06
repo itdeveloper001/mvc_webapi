f2606e7c0a0e60fee38f2207c263c4e27480db89
itdeveloper001 <aqibaptechsfc105@gmail.com>
2017-08-07 02:07:51 +0500

    Insert

---
+++ MyWcfService/MyWcfService/IService1.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace MyWcfService
+{
+    [ServiceContract]
+    public interface IService1
+    {
+      [OperationContract]
+      void employeeRecord(empData ed);
+     }
+    [DataContract]
+    public class empData
+    {
+        int EmpID;
+        string FName;
+        string LName;
+        int Age;
+        string Dep;
+        string DOJ;
+        int EmpSalary;
+        string EmpInfo;
+
+        [DataMember]
+        public int EmployeeNo
+        {
+            set { EmpID = value; }
+            get { return EmpID; }
+        }
+
+        [DataMember]
+        public string FirstName
+        {
+            set { FName = value; }
+            get { return FName; }
+        }
+
+        [DataMember]
+        public string LastName
+        {
+            set { LName = value; }
+            get { return LName; }
+        }
+
+        [DataMember]
+        public int EmpAge
+        {
+            set { Age = value; }
+            get { return Age; }
+        }
+
+        [DataMember]
+        public string EmpDepartement
+        {
+            set { Dep = value; }
+            get { return Dep; }
+        }
+
+        [DataMember]
+        public string EmpDateOFJoining
+        {
+            set { DOJ = value; }
+            get { return DOJ; }
+        }
+
+        [DataMember]
+        public int EmpBasicSalary
+        {
+            set { EmpSalary = value; }
+            get { return EmpSalary; }
+        }
+
+        [DataMember]
+        public string EmployeeInfo
+        {
+            set { EmpInfo = value; }
+            get { return EmpInfo; }
+        }
+
+    }
+
+    }
+
---
+++ MyWcfService/MyWcfService/Service1.svc.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+
+namespace MyWcfService
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Service1" in code, svc and config file together.
+    // NOTE: In order to launch WCF Test Client for testing this service, please select Service1.svc or Service1.svc.cs at the Solution Explorer and start debugging.
+    public class Service1 : IService1
+    {
+       SqlConnection con = new SqlConnection(@"Data Source=(LocalDB)\v11.0;AttachDbFilename='c:\users\maqibc\documents\visual studio 2013\Projects\MyWcfService\MyWcfService\App_Data\EMP.mdf';Integrated Security=True");
+
+
+       public void employeeRecord(empData ed)
+       {
+           SqlCommand cmd = new SqlCommand("INSERT INTO TblEmpRecord(EmpID,FName,LName,Age,Department,DOJ,EmpSalary,EmpInfo)values(@EmpID,@FName,@LName,@Age,@Dep,@DOJ,@EmpSalary,@EmpInfo)", con);
+           cmd.Parameters.AddWithValue("@EmpID", ed.EmployeeNo);
+           cmd.Parameters.AddWithValue("@FName", ed.FirstName);
+           cmd.Parameters.AddWithValue("@LName", ed.LastName);
+           cmd.Parameters.AddWithValue("@Age", ed.EmpAge);
+           cmd.Parameters.AddWithValue("@Dep", ed.EmpDepartement);
+           cmd.Parameters.AddWithValue("@DOJ", ed.EmpDateOFJoining);
+           cmd.Parameters.AddWithValue("@EmpSalary", ed.EmpBasicSalary);
+           cmd.Parameters.AddWithValue("@EmpInfo", ed.EmployeeInfo);
+           con.Open();
+           cmd.ExecuteNonQuery();
+           con.Close();
+          
+       }
+    }
+}
